6d357ca8dc6a4aa0e138809b13deba67b37a95d4
jinwoo1234 <skdiwlsdn5@naver.com>
2024-05-02 17:32:32 +0900

    Fix: WebView 방식 변경

--- android/app/src/main/java/com/example/catchculture_app/MainActivity.java
+++ android/app/src/main/java/com/example/catchculture_app/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.catchculture_app;
+import android.webkit.CookieManager;
 
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    public void onCreate() {
+
+        // 쿠키 허용
+        CookieManager cookieManager = CookieManager.getInstance();
+        cookieManager.setAcceptCookie(true);
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,158 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:webview_flutter/webview_flutter.dart';
-
-final homeUrl = Uri.parse(('https://catch-culture.netlify.app'));
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeScreen(),
+    home: InAppWebViewScreen(),
   ));
 }
 
-class HomeScreen extends StatelessWidget {
-  WebViewController controller = WebViewController()
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..loadRequest(homeUrl)
-    ..setUserAgent('userAgent');
 
-  HomeScreen({Key? key}) : super(key: key);
+class InAppWebViewScreen extends StatefulWidget {
+  const InAppWebViewScreen({Key? key}):super(key:key);
+
+  @override
+  State<InAppWebViewScreen> createState() => _InAppWebViewScreenState();
+}
+
+class _InAppWebViewScreenState extends State<InAppWebViewScreen> {
+  final GlobalKey webViewKey = GlobalKey();
+  Uri myUrl = Uri.parse("https://catch-culture.netlify.app/");
+  late final InAppWebViewController webViewController;
+  late final PullToRefreshController pullToRefreshController;
+  double progress = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    pullToRefreshController = (kIsWeb
+        ? null
+        : PullToRefreshController(
+      options: PullToRefreshOptions(color: Colors.blue,),
+      onRefresh: () async {
+        if (defaultTargetPlatform == TargetPlatform.android) {
+          webViewController.reload();
+        } else if (defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.macOS) {
+          webViewController.loadUrl(urlRequest: URLRequest(url: await webViewController.getUrl()));}
+      },
+    ))!;
+  }
 
   @override
   Widget build(BuildContext context) {
-    ModalRoute.of(context)?.addScopedWillPopCallback(() async {
-      var canGoBack = await controller.canGoBack();
-      if (canGoBack) {
-        controller.goBack();
-        return false; // Return false to prevent default back navigation
-      } else {
-        showDialog(
-          context: context,
-          builder: (context) => AlertDialog(
-            title: Text('앱 종료'),
-            content: Text('앱이 종료됩니다.'),
-            actions: [
-              TextButton(
-                onPressed: () {
-                  Navigator.of(context).pop(false); // Return false to cancel the pop
-                },
-                child: Text('아니오'),
-              ),
-              TextButton(
-                onPressed: () {
-                  SystemNavigator.pop();
-                },
-                child: Text('예'),
-              ),
-            ],
-          ),
-        );
-        return true; // Return true to allow the back navigation
-      }
-    });
-
-    return SafeArea(
-      child: Scaffold(
-        body: WebViewWidget(controller: controller),
-      ),
+    return Scaffold(
+        body: SafeArea(
+            child: WillPopScope(
+                onWillPop: () => _goBack(context),
+                child: Column(children: <Widget>[
+                  progress < 1.0
+                      ? LinearProgressIndicator(value: progress, color: Colors.blue)
+                      : Container(),
+                  Expanded(
+                      child: Stack(children: [
+                        InAppWebView(
+                          key: webViewKey,
+                          initialUrlRequest: URLRequest(url: myUrl),
+                          initialOptions: InAppWebViewGroupOptions(
+                            crossPlatform: InAppWebViewOptions(
+                                javaScriptCanOpenWindowsAutomatically: true,
+                                javaScriptEnabled: true,
+                                useOnDownloadStart: true,
+                                useOnLoadResource: true,
+                                useShouldOverrideUrlLoading: true,
+                                mediaPlaybackRequiresUserGesture: true,
+                                allowFileAccessFromFileURLs: true,
+                                allowUniversalAccessFromFileURLs: true,
+                                verticalScrollBarEnabled: true,
+                                userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36'
+                            ),
+                            android: AndroidInAppWebViewOptions(
+                                useHybridComposition: true,
+                                allowContentAccess: true,
+                                builtInZoomControls: true,
+                                thirdPartyCookiesEnabled: true,
+                                allowFileAccess: true,
+                                supportMultipleWindows: true
+                            ),
+                            ios: IOSInAppWebViewOptions(
+                              allowsInlineMediaPlayback: true,
+                              allowsBackForwardNavigationGestures: true,
+                            ),
+                          ),
+                          pullToRefreshController: pullToRefreshController,
+                          onLoadStart: (InAppWebViewController controller, uri) {
+                            setState(() {myUrl = uri!;});
+                          },
+                          onLoadStop: (InAppWebViewController controller, uri) {
+                            setState(() {myUrl = uri!;});
+                          },
+                          onProgressChanged: (controller, progress) {
+                            if (progress == 100) {pullToRefreshController.endRefreshing();}
+                            setState(() {this.progress = progress / 100;});
+                          },
+                          androidOnPermissionRequest: (controller, origin, resources) async {
+                            return PermissionRequestResponse(
+                                resources: resources,
+                                action: PermissionRequestResponseAction.GRANT);
+                          },
+                          onWebViewCreated: (InAppWebViewController controller) {
+                            webViewController = controller;
+                          },
+                          onCreateWindow: (controller, createWindowRequest) async{
+                            showDialog(
+                              context: context, builder: (context) {
+                              return AlertDialog(
+                                content: SizedBox(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 400,
+                                  child: InAppWebView(
+                                    // Setting the windowId property is important here!
+                                    windowId: createWindowRequest.windowId,
+                                    initialOptions: InAppWebViewGroupOptions(
+                                      android: AndroidInAppWebViewOptions(
+                                        builtInZoomControls: true,
+                                        thirdPartyCookiesEnabled: true,
+                                      ),
+                                      crossPlatform: InAppWebViewOptions(
+                                          cacheEnabled: true,
+                                          javaScriptEnabled: true,
+                                          userAgent: "Mozilla/5.0 (Linux; Android 9; LG-H870 Build/PKQ1.190522.001) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36"
+                                      ),
+                                      ios: IOSInAppWebViewOptions(
+                                        allowsInlineMediaPlayback: true,
+                                        allowsBackForwardNavigationGestures: true,
+                                      ),
+                                    ),
+                                    onCloseWindow: (controller) async{
+                                      if (Navigator.canPop(context)) {
+                                        Navigator.pop(context);
+                                      }
+                                    },
+                                  ),
+                                ),);
+                            },
+                            );
+                            return true;
+                          },
+                        )
+                      ]))
+                ])
+            )
+        )
     );
+  }
 
+  Future<bool> _goBack(BuildContext context) async{
+    if(await webViewController.canGoBack()){
+      webViewController.goBack();
+      return Future.value(false);
+    }else{
+      return Future.value(true);
+    }
   }
 }
